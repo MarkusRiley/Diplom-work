18ee79d6419f0111ea0e80d86f4a4deaf73eea67
Markus Riley <marckriley@yandex.com>
2023-02-18 20:11:57 +0300

    edit file

--- Diplom-work/layout/logo.php
+++
@@ -1,4 +0,0 @@
-<div class="logo">
-    <img src="img/icon.png" style="width: 8.5em;height:8.5em;opacity:1!important;">
-    <div class="logo_name"> Луганский Институт Технологий</div>
-</div>
--- Diplom-work/script_php/content_handler.php
+++
@@ -1,7 +0,0 @@
-<?php
-$num=0;
-$qwer = $db->prepare(" SELECT * FROM `content` WHERE `id_content`!= ?");
-$qwer->execute([$num]);
-$array = $qwer->fetch();
-echo $array[1];
-?>
